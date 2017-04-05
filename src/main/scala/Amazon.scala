c120643f1dcd714808d827af85bf3b1fa87bba0a
Artuwok1986 <artem.vlasenko@gmail.com>
2017-04-05 21:00:43 +0300

    updates

--- src/main/scala/Amazon.scala
+++ src/main/scala/Amazon.scala
@@ -1,27 +1,14 @@
-import argonaut.Parse
+
 import com.ning.http.client
 import dispatch.{Http, url}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
-import org.json4s.JsonAST._
-import dispatch._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
-/*
-1) Finding 1000 most active users (profile names)
-
-2) Finding 1000 most commented food items (item ids).
-
-3) Finding 1000 most used words in the reviews*/
-
-// https://www.cloudera.com/documentation/enterprise/5-5-x/topics/spark_develop_run.html
-
-
-// TODO fix parsing to Int
 case class Review(Id: String, ProductId: String, UserId: String, ProfileName: String, text: String)
 
 object Amazon {
@@ -37,19 +24,21 @@ object Amazon {
     svc.addParameter("text", r.text)
 
     val response: Future[client.Response] = Http(svc)
-    response.onComplete {
+    var x: String = ""
+     response.onComplete {
       case Success(content) => {
-        println("Successful response" + content)
+       x = content.getResponseBody.substring(content.getResponseBody.indexOf("text"))
       }
       case Failure(t) => {
-        println("An error has occurred: " + t.getMessage)
+        t.getMessage
       }
     }
-    Review(r.Id, r.ProductId, r.UserId, r.ProfileName, " ")
+    Review(r.Id, r.ProductId, r.UserId, r.ProfileName, x)
   }
 
   def main(args: Array[String]): Unit = {
 
+    val source = args(0)
     def mapper(line: String): Review = {
       val fields = line.split(',')
       val review: Review = Review(fields(0), fields(1), fields(2), fields(3), fields(9))
@@ -64,15 +53,12 @@ object Amazon {
 
     spark.conf.set("spark.executor.memory", "0.5g")
 
-
     import spark.implicits._
-    val lines: RDD[String] = spark.sparkContext.textFile("/mnt/01D0AD212457E350/projects/amazon/src/resources/Reviews.csv")
+    val lines: RDD[String] = spark.sparkContext.textFile(source)
     val review: Dataset[Review] = lines.map(mapper).toDS().cache()
-
 
     println("Here is our inferred schema:")
     review.printSchema()
-
 
     println("Let's select 1000 most active users:")
     review.groupBy("ProfileName").count().sort(desc("count")).show(1000)
@@ -89,8 +75,7 @@ object Amazon {
       .toDF() // Convert to DataFrame to perform aggregation / sorting
       .groupBy($"value") // Count number of occurences of each word
       .agg(count("*") as "numOccurances")
-      .orderBy($"numOccurances" desc).show() // Show most common words first
-
+      .orderBy($"numOccurances" desc).show(1000) // Show most common words first
 
     val z: RDD[Review] = review.rdd
       z.map((r: Review) => makeRequest(r, z.getNumPartitions)).count()
